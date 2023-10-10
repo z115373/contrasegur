e21e0caa1e756fec28a866e495e97efb1589b490
z115373 <123448646+z115373@users.noreply.github.com>
2023-10-10 14:53:28 +0200

    Update contrasegur.js

--- contrasegur.js
+++ contrasegur.js
@@ -1,7 +1,10 @@
-
-
 function Disdisable() {
     var password = document.getElementById("contrasenya").value;
+    document.getElementById("carac").checked = false;
+    document.getElementById("nume").checked = false;
+    document.getElementById("espe").checked = false;
+    document.getElementById("maju").checked = false;
+    document.getElementById("minus").checked = false;
     document.getElementById("button").disabled = false;
     window.alert(password);
     if (password.length >= 8) {
@@ -12,14 +15,29 @@ function Disdisable() {
         if (lletra >= '0' && lletra <= '9') {
             document.getElementById("nume").checked = true;
         } else {
-            if (lletra == lletra.toUpperCase()) {
-                document.getElementById("maju").checked = true;
+            if (lletra == lletra.toUpperCase()&& lletra == lletra.toLowerCase()) {
+                document.getElementById("espe").checked = true;
+            } else {
+                if (lletra == lletra.toUpperCase()) {
+                    document.getElementById("maju").checked = true;
+                } else {
+                    if (lletra == lletra.toLowerCase()) {
+                        document.getElementById("minus").checked = true;
+                    } 
+                }
             }
         }
-    } 
-}
+    }
+    base=0;
+    if(document.getElementById("nume").checked == true){base=base+10;}
+    if(document.getElementById("espe").checked == true){base=base+41;}
+    if(document.getElementById("maju").checked == true){base=base+40;}
+    if(document.getElementById("minus").checked == true){base=base+40;}
+    nivell=base^password.length/16;
+    window.alert(nivell);
+    document.getElementById("nivell").value=nivell.toString();
+    }
+function Evaluacion() {
 
-function Evaluacion(){
 
-    
 }
